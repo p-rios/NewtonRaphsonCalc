2f0a92894040133d1f508cc4561130cbec086341
Pedro <ph.rios.dev@gmail.com>
2022-11-27 20:52:47 -0300

    Initial commit

---
+++ Funcao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+
+namespace NewtonRaphsonCalc
+{
+    public class Funcao
+    {
+        public SymbolicExpression Expressao { get; set; }
+        public string Derivada { get; set; }
+
+        public Funcao(SymbolicExpression expressao)
+        {
+            Expressao = expressao;
+        }
+
+        public string Derivar()
+        {
+            var x = Expr.Variable("x");
+            //SymbolicExpression func = 3 * (x * x * x) + 2 * x - 6;
+            //Console.WriteLine("f(x) = " + func.ToString());
+
+            var derivada = Expressao.Differentiate(x);
+            return derivada.ToString();
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace NewtonRaphsonCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("1 - Escolher minha propria \n 2- função c1 pré-gerada");
+            Console.WriteLine("Digite a func: ");
+            Funcao funcao = new Funcao(Console.ReadLine());
+      
+            funcao.Derivada = funcao.Derivar();
+            Console.WriteLine(funcao.Derivada);
+                 
+
+
+            //Console.WriteLine("Escolha o epsilon ");
+            //int epsilon = Convert.ToInt16(Console.ReadLine());
+        } 
+
+     
+    }
+}
