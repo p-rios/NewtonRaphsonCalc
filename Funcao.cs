49f677ad73e7fe5988566cdddce48762a81d4188
PedroRios <p_rios@outlook.com>
2023-01-06 09:18:38 -0300

    aa

--- Funcao.cs
+++ Funcao.cs
@@ -4,36 +4,89 @@ using System.Text;
 using MathNet.Symbolics;
 using MathNet.Numerics;
 using Expr = MathNet.Symbolics.SymbolicExpression;
+using System.Linq;
+using MathNet.Numerics.Integration;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace NewtonRaphsonCalc
 {
     public class Funcao
     {
-        public Expr Expressao { get; set; }       
+        public List<double> Coeficientes{ get; set; }
 
-        public Funcao(Expr expressao)
+
+        public Funcao(List<double> coeficientes)
         {
-            Expressao = expressao;
+            Coeficientes= coeficientes;
         }
 
-        public double DerivarNoPonto(double ponto)
+        public List<double> Briot(double ponto)
         {
-            var variables = new Dictionary<string, FloatingPoint>
+           
+            var listaBriot = new List<double>();
+            int i = 0;
+            int UltimoGrau = Coeficientes.Count;
+            foreach (double coeficiente in Coeficientes)
+            {
+                if (i == 0) 
+                { listaBriot.Add(coeficiente);
+                    i++;
+                }
+                if(i == 1)
                 {
-                    { "x", ponto }
-                };
-          
-            var x = Expr.Variable("x");
-            return Expressao.Differentiate(x).Evaluate(variables).RealValue;                             
+
+                    int j = i - 1;
+                    double quociente = Coeficientes[j] * ponto + Coeficientes[i];
+                    listaBriot.Add(quociente);
+                    i++;
+
+
+                }
+
+                if(i>1 && i<UltimoGrau)
+                {
+                    int j = i - 1;
+                    double quociente = listaBriot[j] * ponto + Coeficientes[i];
+                    listaBriot.Add(quociente);
+                    i++;
+                }
+            }
+            return listaBriot; 
+        }
+
+         public double DerivarNoPonto(double ponto)
+        {
+            var listaQuocientes = new List<double>();
+            int k = 2;
+            int UltimoGrau = Coeficientes.Count;
+           
+            foreach (double q in Briot(ponto))
+            {
+                double _quociente = Math.Pow(ponto,UltimoGrau -k) * q;
+                listaQuocientes.Add(_quociente);
+                k++;
+              
+            }
+
+            listaQuocientes.RemoveAt(listaQuocientes.Count - 1);
+            double somaLista = listaQuocientes.Sum();
+           
+            return somaLista;
         }
 
         public double AplicarNoPonto(double ponto)
         {
-            var variables = new Dictionary<string, FloatingPoint>
-                {
-                    { "x", ponto }
-                };
-            return Expressao.Evaluate(variables).RealValue;
+            int k = 1;
+            var listaPontos = new List<double>();
+            int UltimoGrau = Coeficientes.Count;
+            foreach (var coeficiente in Coeficientes)
+            {
+                var result = Math.Pow(ponto, UltimoGrau - k) * coeficiente;
+                listaPontos.Add(result);
+                k++;
+            }
+            return listaPontos.Sum();
         }
 
         public double MetodoNewton(double ponto)
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
 namespace NewtonRaphsonCalc
 {
     internal class Program
@@ -6,7 +9,7 @@ namespace NewtonRaphsonCalc
         static void Main(string[] args)
         {
             Console.WriteLine("Função: ");
-            Funcao funcao = new Funcao(Console.ReadLine());
+            Funcao funcao = new Funcao(Console.ReadLine().Split(' ').Select(double.Parse).ToList()) ;
 
             Console.WriteLine("Aproximação: ");
             double erro = Convert.ToDouble(Console.ReadLine());
@@ -14,26 +17,32 @@ namespace NewtonRaphsonCalc
             Console.WriteLine("Intervalo(valorMin valorMax): ");
             string[] intervalos = Console.ReadLine().Split();
 
+            Console.WriteLine("Maximo de iterações: ");
+            int iteracoes = Convert.ToInt32(Console.ReadLine());
+
             int minVal = int.Parse(intervalos[0]);
             int maxVal = int.Parse(intervalos[1]);
 
             var random = new Random();
             double x0 = random.Next(minVal, maxVal);
-          
+
+            int i = 0;
+
             while(true)
             {
                 var x1 = funcao.MetodoNewton(x0);
                 var x2 = funcao.MetodoNewton(x1);
 
-                if (erro > Math.Abs(x1 - x2)) { 
+                if (erro > Math.Abs(x1 - x2) ^ i == iteracoes) { 
                     Console.WriteLine(@"Raiz da função :" + x2.ToString());
                     break;
                 };
 
                 x0 = x2;
+                i++;
             }
-                 
 
+            Console.Read();
         } 
 
      
